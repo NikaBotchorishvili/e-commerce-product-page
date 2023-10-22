e16b7ec7e7e841ec71946edd6a27ff8dee6fe799
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2023-10-22 12:57:54 +0400

    tailwind commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
+		colors: {
+			// ### Primary
+
+			Orange: "hsl(26, 100%, 55%)",
+			PaleOrange: "hsl(25, 100%, 94%)",
+
+			// ### Neutral
+
+			VeryDarkBlue: "hsl(220, 13%, 13%)",
+			DarkGrayishBlue: "hsl(219, 9%, 45%)",
+			GrayishBlue: "hsl(220, 14%, 75%)",
+			LightGrayishBlue: "hsl(223, 64%, 98%)",
+			White: "hsl(0, 0%, 100%)",
+
+			// with 75% opacity for lightbox background
+			Black: "hsl(0, 0%, 0%)",
+		},
+        fontFamily: {
+            KumbhSans: "'Kumbh Sans', sans-serif",
+        },
 		extend: {},
 	},
 	plugins: [],
